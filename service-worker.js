bb0ff180c7a4436a55a1ab74ea05ea52283087b3
pollorenzo-pixel <pollorenzo103093@gmail.com>
2026-05-06 14:07:46 +0100

    Harden service worker caching and update flow (#101)

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,77 @@
-const CACHE_NAME = "ataraxia-v4";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-
-  // meditation audio
-  "./audio/introduction audio 2.mp3",
-  "./audio/Breath only meditation foundation meditation.mp3",
-  "./audio/body awareness meditation.mp3",
-  "./audio/Thought awareness meditation.mp3",
-  "./audio/Emotional Awareness meditations.mp3",
-  "./audio/deep focus meditation.mp3",
-  "./audio/ending audio foundation.mp3"
+const CACHE_NAME = 'ataraxia-v5';
+const APP_SHELL_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-// Install
-self.addEventListener("install", (event) => {
+const AUDIO_CACHE = [
+  './audio/introduction audio 2.mp3',
+  './audio/Breath only meditation foundation meditation.mp3',
+  './audio/body awareness meditation.mp3',
+  './audio/Thought awareness meditation.mp3',
+  './audio/Emotional Awareness meditations.mp3',
+  './audio/deep focus meditation.mp3',
+  './audio/ending audio foundation.mp3'
+];
+
+const URLS_TO_CACHE = [...APP_SHELL_CACHE, ...AUDIO_CACHE];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-// Fetch
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
   );
+  self.clients.claim();
 });
 
-// Activate (delete old caches)
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  const isNavigation = event.request.mode === 'navigate';
+
+  if (isNavigation) {
+    event.respondWith(
+      fetch(event.request)
+        .then((networkResponse) => {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', responseClone));
+          return networkResponse;
         })
-      );
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        }
+        return networkResponse;
+      });
     })
   );
 });
